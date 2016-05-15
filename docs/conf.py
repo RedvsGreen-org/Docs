0006a5781eae6dc67928802c6cd3e04feb2a08d5
RedvsGreen <info@redvsgreen.com>
2016-05-15 14:52:14 +0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'tutos'
-copyright = '2014, kotrfa'
+project = 'redvsgreen'
+copyright = '2016, redvsgreen-admin'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -177,7 +177,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'tutosdoc'
+htmlhelp_basename = 'redvsgreen'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -197,8 +197,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'tutos.tex', 'tutos Documentation',
-   'kotrfa', 'manual'),
+  ('index', 'redvsgreen.tex', 'RedvsGreen Documentation',
+   'redvsgreen-admin', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -227,8 +227,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'tutos', 'tutos Documentation',
-     ['kotrfa'], 1)
+    ('index', 'redvsgreen', 'RedvsGreen Documentation',
+     ['redvsgreen-admin'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -241,8 +241,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'tutos', 'tutos Documentation',
-   'kotrfa', 'tutos', 'One line description of project.',
+  ('index', 'redvsgreen', 'RedvsGreen Documentation',
+   'redvsgreen-admin', 'redvsgreen', 'One line description of project.',
    'Miscellaneous'),
 ]
 
